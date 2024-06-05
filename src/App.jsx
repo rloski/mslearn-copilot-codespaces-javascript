fc2bf59ae99d45e7c2e2fc952d5b87dae1ccaa8b
Russ Loski <rloski@hotmail.com>
2024-06-05 18:39:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Russ Loski",
+  title: "Database Developer",
   email: "alex@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "russloski",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
